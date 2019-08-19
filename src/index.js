eee9ec0f11fb1b56c3fa1078f0db8046696cc385
james herrero <jamesherrero@gmail.com>
2019-08-19 21:45:37 +1000

    init

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+const app = (
+  <div>
+    <h1>Hello World</h1>
+  </div>
+)
+
+ReactDom.render(app, document.getElementById('root'));
